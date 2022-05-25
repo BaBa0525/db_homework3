3a4afbeb100911b870b5ac46e2733347cf40fd3e
alan910127 <alan469357@gmail.com>
2022-05-25 21:54:39 +0800

    remove <component>, use router instead.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,31 +1,18 @@
-<script setup lang="ts">
-import { ref, computed, defineAsyncComponent } from 'vue';
-import LoginPage from './components/LoginPage.vue';
-
-enum ComponentIndex {
-  loginPage,
-  homePage,
-};
-
-const activeIndex = ref(ComponentIndex.loginPage);
-const asyncHomePage = defineAsyncComponent(() => import('./components/HomePage.vue'));
-
-const activeComponent = computed(() => {
-  switch (activeIndex.value) {
-    case ComponentIndex.loginPage: return LoginPage;
-    case ComponentIndex.homePage: return asyncHomePage;
-  }
-});
-</script>
-
 <template>
-  <h3 @click="activeIndex = ComponentIndex.loginPage">Login</h3>
-  <h3 @click="activeIndex = ComponentIndex.homePage">Home</h3>
-  <component :is="activeComponent" />
+  <router-view />
 </template>
 
-<style scoped lang="scss">
-h3:hover {
-  cursor: pointer;
-}
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+import { useUserStore } from "@/stores/user";
+
+const store = useUserStore();
+const router = useRouter();
+
+if (store.isLoggedIn) router.push({ name: 'home' });
+else router.push({ name: 'signin' });
+</script>
+
+<style lang="scss">
+@import "@/styles/global.scss";
 </style>
