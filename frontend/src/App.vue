34f0e65c8da6c597f6bd455ac8c41bbfc88b911e
alan910127 <alan469357@gmail.com>
2022-05-25 16:48:19 +0800

    Simple implementation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,31 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, computed, defineAsyncComponent } from 'vue';
+import LoginPage from './components/LoginPage.vue';
 
-const name = ref('World');
+enum ComponentIndex {
+  loginPage,
+  homePage,
+};
+
+const activeIndex = ref(ComponentIndex.loginPage);
+const asyncHomePage = defineAsyncComponent(() => import('./components/HomePage.vue'));
+
+const activeComponent = computed(() => {
+  switch (activeIndex.value) {
+    case ComponentIndex.loginPage: return LoginPage;
+    case ComponentIndex.homePage: return asyncHomePage;
+  }
+});
 </script>
 
 <template>
-  <h1>Hello, {{ name }}!</h1>
+  <h3 @click="activeIndex = ComponentIndex.loginPage">Login</h3>
+  <h3 @click="activeIndex = ComponentIndex.homePage">Home</h3>
+  <component :is="activeComponent" />
 </template>
 
-<style>
+<style scoped lang="scss">
+h3:hover {
+  cursor: pointer;
+}
 </style>
