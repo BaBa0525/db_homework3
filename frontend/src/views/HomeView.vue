af4db6ead101b5d44bcff746579165e741204108
alan910127 <alan469357@gmail.com>
2022-05-25 21:53:53 +0800

    Move to view

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,9 @@
-<script setup lang="ts">
-</script>
-
 <template>
     <h1>Home Page</h1>
 </template>
+
+<script setup lang="ts">
+</script>
 
 <style>
 </style>
---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,35 @@
+<template>
+    <router-link to="/signin" class="router-link">Sign In</router-link>
+    <router-link to="/signup" class="router-link">Sign Up</router-link>
+    <router-view></router-view>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+</script>
+
+<style scoped lang="scss">
+.router-link {
+    font-size: 20px;
+    color: gray;
+    text-decoration: none;
+    transition: 0.3s all ease;
+
+    &:hover {
+        color: black;
+        font-size: 30px;
+    }
+}
+
+.router-link-exact-active {
+    color: black;
+    text-decoration: none;
+    font-size: 26px;
+
+    &:hover {
+        font-size: 26px;
+    }
+}
+</style>
