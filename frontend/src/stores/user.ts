26fc2c16be65806b298f7ebf52123635ec05cd1c
alan910127 <alan469357@gmail.com>
2022-05-25 21:55:55 +0800

    add isEmptyUser method

--- frontend/src/stores/user.ts
+++ frontend/src/stores/user.ts
@@ -1,21 +1,19 @@
 import { defineStore } from "pinia";
 import axios from "axios";
 
+function isEmptyUser(user: User) {
+  for (const _ in user) {
+    return false;
+  }
+  return true;
+}
+
 export const useUserStore = defineStore("user", {
   state: () => ({
-    user: {
-      account: undefined,
-      phone: undefined,
-      password: undefined,
-      realname: undefined,
-      role: undefined,
-      shopname: undefined,
-      latitude: undefined,
-      longitude: undefined,
-      balance: undefined,
-    } as User
+    user: {} as User,
   }),
   getters: {
+    isLoggedIn: (state) => !isEmptyUser(state.user),
     account: (state) => state.user.account,
   },
   actions: {
@@ -23,7 +21,7 @@ export const useUserStore = defineStore("user", {
       this.user = user;
     },
     addMoney(amount: number) {
-      if (this.user.balance === undefined) this.user.balance = amount;
+      if (!this.user.balance) this.user.balance = amount;
       else this.user.balance += amount;
     },
     async setPosition(latitude: number, longitude: number) {
