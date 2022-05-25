dbb47fb13293f8dca7ab162830230727bd7c85c4
alan910127 <alan469357@gmail.com>
2022-05-25 21:55:37 +0800

    add routes

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,9 +1,31 @@
 import { createRouter, createWebHistory } from "vue-router";
+import LoginView from "@/views/LoginView.vue";
+import SignInPage from "@/components/SignInPage.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-
+    {
+      name: 'login',
+      path: '/login',
+      component: LoginView,
+      children: [
+        {
+          name: 'signin',
+          path: '/signin',
+          component: SignInPage
+        },
+        {
+          name: 'signup',
+          path: '/signup',
+          component: () => import('@/components/SignUpPage.vue'),
+        },
+      ],
+    }, {
+      name: 'home',
+      path: '/home',
+      component: () => import('@/views/HomeView.vue'),
+    }
   ],
 });
 
