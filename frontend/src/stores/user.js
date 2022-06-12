58e0059743642acea037906a00ad5159d5cc1e95
alan910127 <alan469357@gmail.com>
2022-06-13 03:18:47 +0800

    pinia persisted state

--- frontend/src/stores/user.js
+++ frontend/src/stores/user.js
@@ -2,6 +2,7 @@ import { defineStore } from 'pinia';
 
 export const useUserStore = defineStore({
     id: 'user',
+    persist: true,
     state: () => ({
         account: '',
         password: '',
